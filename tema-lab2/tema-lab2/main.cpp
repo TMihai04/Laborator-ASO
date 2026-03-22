569c8d078af2d89212dfb743eae2694ea0a97b0a
microslop <tmihai0404@gmail.com>
2026-03-22 16:49:07 +0200

    Read all subkeys from a registry

--- tema-lab2/tema-lab2/main.cpp
+++ tema-lab2/tema-lab2/main.cpp
@@ -1,22 +1,32 @@
+#include <stdio.h>
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <Windows.h>
+#include <winreg.h>
+#include <winerror.h>
 
 int main() {
-	std::string s_InputFilePath;
+	HKEY regHandle = NULL;
+	//char regPath[]  = "HARDWARE\\ACPI\\SSDT\\VBOX__\\VBOXCPUT\\00000002";
+	char regPath[] = "SOFTWARE";
+	//char regPath[] = "SOFTWARE\\Mozilla\\Firefox\\TaskBarIDs";
+	LSTATUS openStatus = RegOpenKeyExA(HKEY_LOCAL_MACHINE, (LPCSTR)regPath, 0, KEY_READ | KEY_WOW64_64KEY, &regHandle);
 
-	std::cout << "Input file path: ";
-	getline(std::cin, s_InputFilePath);
-
-	std::ifstream if_InputFile(s_InputFilePath);
-	if (!if_InputFile.good()) {
-		std::cout << "File does not exist" << std::endl;
+	if (openStatus != ERROR_SUCCESS) {
+		std::cout << "Failed to open registry: " << openStatus << std::endl;
 		return -1;
 	}
 
-	std::string s_Line;
-	while (getline(if_InputFile, s_Line)) {
-		std::cout << s_Line << std::endl;
+	std::cout << "Opened " << regPath << ":" << std::endl;
+
+	DWORD subkeyIdx = 0;
+	char subkeyName[256] = { 0 };
+	DWORD nameLen = sizeof(subkeyName);
+
+	while (RegEnumKeyExA(regHandle, subkeyIdx, subkeyName, &nameLen, NULL, NULL, NULL, NULL) != ERROR_NO_MORE_ITEMS) {
+		std::cout << subkeyIdx << ": " << subkeyName << std::endl;
+		subkeyIdx++;
+		nameLen = sizeof(subkeyName);
 	}
 
 	return 0;
