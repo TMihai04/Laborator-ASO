8f8c6b5a393a935cc00df6eb909eed1dfd2ce24b
microslop <tmihai0404@gmail.com>
2026-05-17 16:06:29 +0300

    Simple to do app

--- README.md
+++
@@ -1,3 +0,0 @@
-# Laborator-ASO
-
-Temele si exercitiile de la laboratorul de ASO. Fiecare branch va avea activitatea pentru laboratorul din saptamana respectiva.
---
+++ todo_app/todo_app/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo_app.Models;
+
+namespace todo_app.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
